bb89d396bbb38afadc232a151efd6ea234401943
Amanda Ellen <141930525+AmandaEllenBR@users.noreply.github.com>
2023-10-10 08:43:01 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("#form")
+const name = document.querySelector("#name")
+const email = document.querySelector("#email")
+const celular = document.querySelector("#number")
+const selecione = document.querySelector("#selecione")
+
+//console.log(form, name, email, number, selecione )
+
